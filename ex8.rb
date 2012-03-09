cc1e42016b3bd6e1b8c0aa4ff764119765846601
Kimbeaux <KimbeauxE@verizon.net>
2012-03-09 15:48:21 -0500

    ex 8, ec is noop

---
+++ ex8.rb
@@ -0,0 +1,6 @@
+formatter = "%s %s %s %s"
+
+puts formatter % [1, 2, 3, 4]
+puts formatter % ["one", "two", "three", "four"]
+puts formatter % [formatter, formatter, formatter, formatter]
+puts formatter % ["I had this thing.", "That you could type up right.", "But it didn't sing.", "So I said goodnight."]
